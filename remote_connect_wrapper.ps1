141b08041980d34b959b3569ffc93b23f7e69ea0
DebarchanaH <debarchana.halder@tcs.com>
2019-03-17 04:07:10 +0000

    print session

--- remote_connect_wrapper.ps1
+++ remote_connect_wrapper.ps1
@@ -8,5 +8,5 @@ $targetname = Get-Content "D:\Password\targetname.txt"
 #Enter-PSSession $targetname -Credential $Credentialvm
 
 $s = New-PSSession -ComputerName $targetname -Credential $Credentialvm
-
+$s
 Invoke-Command -Session $s -FilePath D:\Password\awstoazure.ps1
