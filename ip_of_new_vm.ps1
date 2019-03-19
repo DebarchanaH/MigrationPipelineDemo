47b32ed958637554e96fc16594e01c419eb2854b
DebarchanaH <debarchana.halder@tcs.com>
2019-03-19 10:13:48 +0000

    new file

---
+++ ip_of_new_vm.ps1
@@ -0,0 +1,12 @@
+$username = Get-Content "D:\Password\username.txt"
+$Secure = Get-Content "D:\Password\Password.txt"
+$SecurePassword = ConvertTo-SecureString $Secure -AsPlainText -Force
+$Credential = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $SecurePassword
+Login-AzureRmAccount -Credential $Credential
+
+$RGName = Get-Content "D:\Password\resourcegroupname.txt"
+$RG = Get-AzureRmResourceGroup -Name $RGName
+
+$ipName = Get-Content "D:\Password\pip.txt"
+$publicIp = Get-AzureRmPublicIpAddress -Name $ipName -ResourceGroupName $RG.ResourceGroupName
+$publicIp.IpAddress | Out-File -FilePath D:\Password\azurenewip.txt
